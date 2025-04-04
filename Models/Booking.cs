d307d48a3beceeb3086c9abdbb776a2a138cc21c
Lars <lav005@edu.zealand.dk>
2025-04-04 11:06:44 +0200

    Added Booking.cs in Models folder

---
+++ Models/Booking.cs
@@ -0,0 +1,12 @@
+﻿namespace DSVInternBooking.Models
+{
+    public class Booking
+    {
+
+        public Booking(DateTime dateTimeBegin, DateTime dateTimeEnd, Room room)
+            => (DateTimeBegin, DateTimeEnd, Room) = (dateTimeBegin, dateTimeEnd, room);
+        public DateTime DateTimeBegin { get; set; }
+        public DateTime DateTimeEnd { get; set; }
+        public Room Room { get; set; }
+    }
+}
