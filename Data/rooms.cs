32cf7b87b61475c324e3a4fd3be9065327617e8f
Lars <lav005@edu.zealand.dk>
2025-04-04 10:56:47 +0200

    Revert "removed unnessesary comment"
    This reverts commit a19a1022356c5dd4583efacaf17f8b8c5ac32849.

--- Data/rooms.cs
+++ Data/rooms.cs
@@ -1,10 +1,15 @@
-using System.Reflection.Metadata;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
 public class Rooms
 {
-    public static List<Room> RoomsList { get; private set; } = new List<Room>()
+    // public List<string> Countries { get; private set;} = new List<string>
+    // {
+    //     "Danmark",
+    //     "Tyskland",
+    //     "Sverige",
+    //     "Norge",
+    //     "Nederlandene"
+    // };
+
+    public static List<Room> RoomsList { get; private set; } = new List<Room>
     {
         new("Danmark", 1, 15, new List<string>{"Projekter", "Whiteboard"}),
         new("Danmark", 2, 20, new List<string>{"Whiteboard"}),
@@ -14,15 +19,12 @@ public class Rooms
 
     public Rooms()
     {
-        // Room room = ReadJson<Room>("./Data/rooms.json");
-        // RoomsList.Add(room);
     }
 
-    // private T ReadJson<T>(in string filePath)
-    // {
-    //     string text = File.ReadAllText(filePath);
-    //     return JsonSerializer.Deserialize<T>(text);
-    // }
+    private void CreateRooms()
+    {
+        
+    }
 
     public List<Room> GetRooms()
     {
