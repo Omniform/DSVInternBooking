0ecd314025acfb3585a5824cb181e4271113ce70
lasermorph <stefanashansen@hotmail.com>
2025-04-06 12:34:45 +0200

    Remove of booking added

--- Pages/MyBookings.cshtml.cs
+++ Pages/MyBookings.cshtml.cs
@@ -1,3 +1,4 @@
+using DSVInternBooking.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,30 @@ namespace DSVInternBooking.Pages
 {
     public class MyBookingsModel : PageModel
     {
+        [BindProperty]
+        public string Country { get; set; }
+        [BindProperty]
+        public int ID { get; set; }
+
+        public BookingService BookingService { get; set; }
+
+        public MyBookingsModel(BookingService bookingService)
+        => BookingService = bookingService;
+        
         public void OnGet()
         {
+        }
+
+        public void OnPostRemove()
+        {
+            foreach (Booking booking in BookingService.BookedRooms)
+            {
+                if (Country == booking.Room.Country && ID == booking.Room.ID)
+                {
+                    BookingService.RemoveBooking(booking);
+                    break;
+                }
+            }
         }
     }
 }
